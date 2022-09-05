545c3859cf2ed3be507ef2b47d1e96510af4df81
MRS\toqeer.iqbal <toqeer.iqbal@mrs-electronic.com>
2022-09-05 17:48:47 +0500

    signup + logout done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,112 @@
-import { greetUser } from '$utils/greet';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
+
+import { webflowAuth } from '$utils/config';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  // Initialize Firebase
+  const app = initializeApp(webflowAuth.firebaseConfig);
+
+  // Initialize Firebase Authentication and get a reference to the service
+  const auth = getAuth(app);
+  const user = auth.currentUser;
+
+  const bodyAuth = document.body.getAttribute('data-user-auth');
+  const bodyUnauth = document.body.getAttribute('data-user-unauth');
+  const userAuth = document.querySelectorAll('[data-user-auth]');
+  const userUnauth = document.querySelectorAll('[data-user-unauth]');
+  // const userDisplayName = document.querySelectorAll('[data-user-displayName]');
+  // const userEmail = document.querySelectorAll('[data-user-email]');
+  // const userContent = document.querySelectorAll('[data-user]');
+
+  userAuth.forEach(function (el) {
+    el.style.display = 'none';
+  });
+  userUnauth.forEach(function (el) {
+    el.style.display = 'none';
+  });
+
+  // Signup
+  const signupForms = document.querySelectorAll('[data-signup-form]');
+  const signupErrors = document.querySelectorAll('[data-signup-error]');
+  const signupLoading = document.querySelectorAll('[data-signup-loading]');
+
+  signupForms.forEach(function (el) {
+    const signupEmail = el.querySelector('[data-signup-email]');
+    const signupPassword = el.querySelector('[data-signup-password]');
+
+    el.addEventListener('submit', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      signupErrors.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      signupLoading.forEach(function (el) {
+        el.style.display = 'block';
+      });
+
+      createUserWithEmailAndPassword(auth, signupEmail.value, signupPassword.value)
+        .then((userCredential) => {
+          // Signed in
+          const { user } = userCredential;
+          window.location.href = webflowAuth.signupRedirectPath;
+        })
+        .catch((error) => {
+          signupErrors.forEach(function (el) {
+            el.innerText = error.message;
+            el.style.display = 'block';
+          });
+
+          setTimeout(function () {
+            signupLoading.forEach(function (el) {
+              el.style.display = 'none';
+            });
+          }, 1000);
+        });
+    });
+  });
+
+  onAuthStateChanged(auth, (user) => {
+    if (user && bodyUnauth) {
+      window.location.href = webflowAuth.loginRedirectPath;
+    } else if (!user && bodyAuth) {
+      window.location.href = webflowAuth.loginPath;
+    }
+    if (user) {
+      userAuth.forEach(function (el) {
+        el.style.display = null;
+      });
+      userUnauth.forEach(function (el) {
+        el.style.display = 'none';
+      });
+    } else {
+      userAuth.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      userUnauth.forEach(function (el) {
+        el.style.display = null;
+      });
+    }
+  });
+  // Logout
+  const authLogout = document.querySelectorAll('[data-logout]');
+  authLogout.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      signOut(auth)
+        .then(() => {
+          window.location.href = webflowAuth.logoutRedirectPath;
+        })
+        .catch(function () {});
+    });
+  });
 });
---
+++ src/utils/config.ts
@@ -0,0 +1,23 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+export const webflowAuth = {
+  loginPath: '/log-in',
+  loginRedirectPath: '/user',
+  signupPath: '/sign-up',
+  signupRedirectPath: '/user',
+  logoutRedirectPath: '/',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBhNFLNzExqkp9dSV3gc38LiLqE2ga98XU',
+    authDomain: 'mrs-webflow.firebaseapp.com',
+    databaseURL: 'https://mrs-webflow-default-rtdb.firebaseio.com',
+    projectId: 'mrs-webflow',
+    storageBucket: 'mrs-webflow.appspot.com',
+    messagingSenderId: '52297127254',
+    appId: '1:52297127254:web:c6747c871e0dc286cef2f7',
+    measurementId: 'G-3J00S98ECT',
+  },
+};
